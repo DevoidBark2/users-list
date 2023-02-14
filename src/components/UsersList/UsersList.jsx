68a5ceb50d02323026f5b04ef38fea03268e0586
Devoid_Bark <petros_9002@mail.ru>
2023-02-15 01:18:11 +0300

    Layout UserList component

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,9 +1,42 @@
 import React from "react";
 import "./UsersList.css";
-
+import cancel from '../../images/cancel.svg';
 export const UsersList = () =>{
     return(
-        <div>
+        <div className="users-list-block">
+            <div className="sort-users-block">
+                <p className="sort-title">Сортировка:</p>
+                <p className="sort-item active">Дата регистрации</p>
+                <p className="sort-item">Рейтинг</p>
+            </div>
+            <div className="users-list">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Имя пользователя</th>
+                            <th>E-mail</th>
+                            <th>Дата регистрации</th>
+                            <th>Рейтинг</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Username</td>
+                            <td>test@test.ru</td>
+                            <td>23.09.19</td>
+                            <td>12</td>
+                            <td><img src={cancel} alt="cancel"/></td>
+                        </tr>
+                        <tr>
+                            <td>Username</td>
+                            <td>test@test.ru</td>
+                            <td>23.09.19</td>
+                            <td>12</td>
+                            <td><img src={cancel} alt="cancel"/></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
