1ddc588d8f2bd02ef798b195875f5de99506ab04
Devoid_Bark <petros_9002@mail.ru>
2023-02-14 22:22:50 +0300

    Layout of the SearchUsers component and creation of the UserList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
 import './App.css';
+import {SearchUsers} from "./components/SearchUsers/SearchUsers";
+import {UsersList} from "./components/UsersList/UsersList";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-
+          <h1 className="main-title">Список пользователей</h1>
+          <SearchUsers/>
+          <UsersList/>
       </div>
     </div>
   );
---
+++ src/components/UsersList/UsersList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./UsersList.css";
+
+export const UsersList = () =>{
+    return(
+        <div>
+        </div>
+    )
+}
